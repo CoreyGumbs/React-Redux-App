f874497e3728303a2f288d3b8cf1b5e6095e4ac9
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-23 18:38:20 -0500

    [Initial] Initial File, Redux, Reducer setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import Normalize from 'react-normalize';
+
+import { reducer } from './reducers/reducers';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './css/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store =  createStore(reducer);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+<Provider store={store}>
+    <Normalize />
+    <App />
+</Provider>
+, document.getElementById('root'));
+
+
