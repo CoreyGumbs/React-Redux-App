1fc3834202ede4605a9990c214b9b579d833407d
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-23 23:40:54 -0500

    [Add] Completed Reducer setup, added react-router, SplashPage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-  
+    <div className='App'>
+      
+      <Switch>
+      <Route path='/'>
+
+      </Route>
+    </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Normalize from 'react-normalize';
 
 import combineReducers from './reducers/index';
@@ -12,11 +13,14 @@ import App from './App';
 import './css/index.css';
 
 const store =  createStore(combineReducers, applyMiddleware(thunk, logger));
+console.log(store.getState());
 
 ReactDOM.render(
 <Provider store={store}>
-    <Normalize />
-    <App />
+    <Router>
+        <Normalize />
+        <App />
+    </Router>
 </Provider>
 , document.getElementById('root'));
 
