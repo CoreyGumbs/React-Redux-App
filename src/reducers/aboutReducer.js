f0df5e669734afef8b9e0a99fd8705931bf5a5e5
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-23 22:45:46 -0500

    [Add] Added About and History Reducers

---
+++ src/reducers/aboutReducer.js
@@ -0,0 +1,11 @@
+export const aboutState = {
+    data: []
+}
+
+
+export const aboutReducer = (state = aboutState, action) => {
+    switch(action.type){
+        default:
+            return state
+    }
+}
